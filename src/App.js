f95ef65d1e769bc5a8477d420a52a40cc6d2a1c0
KHW <huynjoo01@naver.com>
2025-03-08 18:09:38 +0900

    add route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Routes, Route, Link } from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -11,8 +13,14 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      {/* <Home/> */}
-      <About/>
+      <nav>
+        <Link to='/'>Home</Link> | <Link to='/about'>About</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+
       <Footer/>
     </div>
   );
