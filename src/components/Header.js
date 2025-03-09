d82c779e2e9f3652eb88ff0868dfc646c14fd72a
KHW <huynjoo01@naver.com>
2025-03-09 12:24:52 +0900

    상태 관리 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,25 @@
+import { useState, useEffect } from 'react';
 
 export default function Header() {
+
+    const [opacity, setOpacity] = useState(1);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+            const newOpacity = 1.5 - currentScrollY / 100;
+            setOpacity(newOpacity);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <header className='header'>
+        <header className='header' style={{opacity: opacity}}>
             <h1>My Website</h1>
         </header>
     );
