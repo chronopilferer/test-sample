678a47bdc78ef628af2cd54f6adb3e8da8bb5602
KHW <huynjoo01@naver.com>
2025-04-06 17:12:57 +0900

    배포 설정

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 import { FaHome } from "react-icons/fa";
 
@@ -12,15 +13,15 @@ function NavigationBar() {
   return (
     <Navbar sticky="top" expand="lg" className="bg-body-tertiary">
       <Container className='nav-container'>
-        <Nav.Link href="/">
+        <Nav.Link as={Link} to="/">
           <FaHome className="nav-icon-link" />
         </Nav.Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/About" className="nav-link-hover-underline">About</Nav.Link>
-            <Nav.Link href="/Project" className="nav-link-hover-underline">Project</Nav.Link>
-            <Nav.Link href="/Contact" className="nav-link-hover-underline">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/About" className="nav-link-hover-underline">About</Nav.Link>
+            <Nav.Link as={Link} to="/Project" className="nav-link-hover-underline">Project</Nav.Link>
+            <Nav.Link as={Link} to="/Contact" className="nav-link-hover-underline">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
