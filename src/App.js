ec448eea30cdc3854006aa5049b528acc4c0f5ea
KHW <huynjoo01@naver.com>
2025-04-06 11:49:54 +0900

    bootstrap 적용 전 프로젝트 정리

--- src/App.js
+++ src/App.js
@@ -2,39 +2,10 @@ import React from 'react';
 
 import { Routes, Route, Link } from 'react-router-dom';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Test from './components/Test';
-import TestArray from './components/TestArray';
-
-import './styles/App.css'
-
 function App() {
-
-  const users = [
-    {id:'1', name:'Alice', age:20},
-    {id:'2', name:'Bob', age:21},
-    {id:'3', name:'Chris', age:22},
-    {id:'4', name:'David', age:23},
-    {id:'5', name:'Ella', age:24}
-  ]
 
   return (
     <div className="App">
-      <Header/>
-      <nav>
-        <Link to='/'>Home</Link> | <Link to='/about'>About</Link>
-      </nav>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-      </Routes>
-
-      <TestArray users={users} /> 
-
-      <Footer/>
 
     </div>
   );
--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() {
-    return (
-    <footer className='footer'>
-        <p> 2025 My Website. All rights reserved.</p>
-     </footer>
-    )
-}
