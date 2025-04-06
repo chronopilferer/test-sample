bf69ed50b78f03843ffae547c0d94cbec3700597
KHW <huynjoo01@naver.com>
2025-04-06 13:32:18 +0900

    라우팅 정의

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 import { Routes, Route, Link } from 'react-router-dom';
 
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 function App() {
 
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+
+      <Link to="/">Home</Link>
+      <Link to="/About">About</Link>
+      <Link to="/Contact">Contact</Link>
 
     </div>
   );
